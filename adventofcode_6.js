65bcb737304c8ab9ddbbcc07ab3e68a165f7ba14
Katrin Krieger <contact@twinrivercode.de>
2021-12-07 20:49:08 +0100

    Day 6 complete

--- adventofcode_6.js
+++ adventofcode_6.js
@@ -18,27 +18,17 @@ const lanternfish = (data, days) => {
 
 const lanternfish2 = (data, days) => {
     let ages = Array(9).fill(0);
-    let dayarr = Array(days - 2).fill(0);
     //init ages
     data.forEach((el) => {
         ages[el]++;
     });
 
-    let population = dayarr.reduce((acc, curr) => {
-        acc.forEach((fish, index, ages) => {
-            if (index === 0 && fish > 0) {
-                ages[index] -= fish;
-                ages[8] += fish;
-                ages[6] += fish;
-            } else if (index > 0 && fish > 0) {
-                ages[index] -= fish;
-                ages[index - 1] += fish;
-            }
-        });
-        console.log(acc.reduce((a, c) => a + c, 0));
-        return acc;
-    }, ages);
-    return population.reduce((acc, curr) => acc + curr, 0);
+    for (let i = 0; i < days; i++) {
+        ages.push(ages.shift());
+        ages[6] += ages[8];
+    }
+
+    return ages.reduce((acc, curr) => acc + curr, 0);
 };
 
 module.exports.lanternfish = lanternfish;
