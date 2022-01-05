b4adc59b5158c997c89e27bf76100ba67ea1ff6c
Katrin Krieger <mail@katrin-krieger.de>
2022-01-05 14:59:32 +0100

    solutions.js adapted

--- solutions.js
+++ solutions.js
@@ -20,11 +20,19 @@ const {
     insertAllLineSegments,
 } = require("./adventofcode_5");
 const { lanternfish, lanternfish2 } = require("./adventofcode_6");
-const { getMinimumFuelHorizontalPosition, getMinimumFuelHorizontalPosition2 } = require("./adventofcode_7");
+const {
+    getMinimumFuelHorizontalPosition,
+    getMinimumFuelHorizontalPosition2,
+} = require("./adventofcode_7");
 const { getScore, getScoreForClosingChars } = require("./adventofcode_10");
 const { getLowPoints, sumOfRisksOfLowPoints } = require("./adventofcode_9");
 const { findAllEasyDigits } = require("./adventofcode_8");
 const { dotsAfterFold } = require("./adventofcode_13");
+const {
+    polymerization,
+    mostCommonElement,
+    leastCommonElement,
+} = require("./adventofcode_14");
 
 console.log(`
 ##############################################################
@@ -79,10 +87,7 @@ let data_day4 = parseInput("./data/day_4.txt", "\n\n", (el) => el);
 
 console.log("Winning bingo score: ", getFirstWinningScore(data_day4));
 data_day4 = parseInput("./data/day_4.txt", "\n\n", (el) => el);
-console.log(
-    "Last winning board score: ",
-    getLastWinningScore(data_day4)
-);
+console.log("Last winning board score: ", getLastWinningScore(data_day4));
 
 console.log("\n************* DAY 5: Hydrothermal Venture **************\n");
 const data_day5 = parseInput("./data/day_5.txt", "\n", (el) =>
@@ -116,26 +121,51 @@ console.log("Minimum fuel 2: ", getMinimumFuelHorizontalPosition2(data_day7));
 
 console.log("\n************* DAY 8: Seven Segment Search **************\n");
 
-let data_day8 = parseInput("./data/day_8.txt", "\n", e => e.trim().split("|"));
+let data_day8 = parseInput("./data/day_8.txt", "\n", (e) =>
+    e.trim().split("|")
+);
 console.log("Easy segments: ", findAllEasyDigits(data_day8));
 
-
 console.log("\n************* DAY 9: Smoke Basin **************\n");
-let data_day9 = parseInput("./data/day_9.txt", "\n", el => el.split("").map(Number));
-console.log("Low points score: ", sumOfRisksOfLowPoints(getLowPoints(data_day9)));
+let data_day9 = parseInput("./data/day_9.txt", "\n", (el) =>
+    el.split("").map(Number)
+);
+console.log(
+    "Low points score: ",
+    sumOfRisksOfLowPoints(getLowPoints(data_day9))
+);
 
 console.log("\n************* DAY 10: Syntax Scoring **************\n");
-let data_day10 = parseInput("./data/day_10.txt", "\n", el => el);
+let data_day10 = parseInput("./data/day_10.txt", "\n", (el) => el);
 console.log("Syntax error score: ", getScore(data_day10));
-console.log("Score for closing brackets: " + getScoreForClosingChars(data_day10));
-
+console.log(
+    "Score for closing brackets: " + getScoreForClosingChars(data_day10)
+);
 
 console.log("\n************* DAY 13: Transparent Origami **************\n");
-let [dots, instructions] = parseInput("./data/day_13.txt", "\n\n", e => e.split("\n"));
-dots = dots.map(pair => pair.split(",").map(Number));
-instructions = instructions.map(i => {
+let [dots, instructions] = parseInput("./data/day_13.txt", "\n\n", (e) =>
+    e.split("\n")
+);
+dots = dots.map((pair) => pair.split(",").map(Number));
+instructions = instructions.map((i) => {
     let arr = i.split(" ");
     return arr[arr.length - 1].split("=");
-})
+});
 console.log("Dots after first fold: " + dotsAfterFold(dots, [instructions[0]]));
 console.log("Dots after last fold: " + dotsAfterFold(dots, instructions));
+
+console.log("\n************* DAY 14: Extended Polymerization **************\n");
+let template;
+[template, instructions] = parseInput("./data/day_14.txt", "\n\n", (e) => e);
+instructions = instructions.split("\n").map((i) => i.split(" -> "));
+
+let polymer = polymerization(template, instructions, 10);
+
+const occurrences = polymer.split("").reduce(function (acc, curr) {
+    return acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc;
+}, {});
+
+let most = occurrences[mostCommonElement(polymer.split(""))];
+let least = occurrences[leastCommonElement(polymer.split(""))];
+console.log(most, least);
+console.log("Most common minus least common elements: ", most - least);
